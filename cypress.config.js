cb34fce5dc0ad136dafeb8712cf7745c6bb0ba85
heloisapaesmantovani <148348852+heloisapaesmantovani@users.noreply.github.com>
2023-10-19 16:51:25 -0300

    Revert "Create E2E Login + Cloud Cypress + Config YML GitHub Run PR Main"

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "n9gidp",
   e2e: {
-    baseUrl: "https://automationpratice.com.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
